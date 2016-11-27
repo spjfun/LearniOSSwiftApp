dc6f40b1b75d01765103666c69c40aa8c21b4ea2
peijyun <peijyun.shih@gmail.com>
2016-11-27 23:05:33 +0800

    0502test 按下按鈕動態改變標籤名稱為按鈕名稱

---
+++ ch0502test/ch0502test/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ch0502test
+//
+//  Created by shihpeijyun on 2016/11/27.
+//  Copyright (c) 2016年 zhizhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+   
+    @IBOutlet weak var myBtnRight: UIButton!
+    @IBOutlet weak var myBtnLeft: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
