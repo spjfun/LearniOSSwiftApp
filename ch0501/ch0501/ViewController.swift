b40bfd6650d98152d546fba9e7ba985b0d8d969e
peijyun <peijyun.shih@gmail.com>
2016-11-27 22:07:51 +0800

    0501 add UILabel 在文字標籤中顯示文字

--- ch0501/ch0501/ViewController.swift
+++ ch0501/ch0501/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myLabel.text = "Hello iOS :)";
     }
 
     override func didReceiveMemoryWarning() {
