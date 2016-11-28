8fe42f2c6e31444eacb095f1c9e87c09d56b97fe
peijyun <peijyun.shih@gmail.com>
2016-11-28 21:35:02 +0800

    0502test 多元件與單一程式碼連結

--- ch0502test/ch0502test/ViewController.swift
+++ ch0502test/ch0502test/ViewController.swift
@@ -24,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapBtn(sender: UIButton) {
+        myLabel.text = sender.currentTitle
+    }
 
 }
 
