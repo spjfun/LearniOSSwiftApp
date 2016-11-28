356eea6bb4eb54da929bd154dbd7e69f27864f33
peijyun <peijyun.shih@gmail.com>
2016-11-28 21:47:22 +0800

    ch0503 UIButton 讓文字標籤顯示輸入文字

---
+++ ch0503/ch0503/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ch0503
+//
+//  Created by shihpeijyun on 2016/11/28.
+//  Copyright (c) 2016年 zhizhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func inputText(sender: AnyObject) {
+    }
+
+}
+
