c4bcb3ee4a16c8eb93a553931d4b6fda97ad762d
peijyun <peijyun.shih@gmail.com>
2016-11-28 22:52:57 +0800

    ch0504test 使用for, while, do-while迴圈 練習插入大量文字

---
+++ ch0504test/ch0504testTests/ch0504testTests.swift
@@ -0,0 +1,36 @@
+//
+//  ch0504testTests.swift
+//  ch0504testTests
+//
+//  Created by shihpeijyun on 2016/11/28.
+//  Copyright (c) 2016年 zhizhi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ch0504testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
