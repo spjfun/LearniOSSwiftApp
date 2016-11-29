ecc96ef67a232b136af82abef0222517a9669758
peijyun <peijyun.shih@gmail.com>
2016-11-29 22:06:54 +0800

    ch0505 UISwitch 利用切換開關顯示文字

--- ch0504test/ch0504test/ViewController.swift
+++ ch0504test/ch0504test/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var forTxtView: UITextView!
+    @IBOutlet weak var whileTxtView: UITextView!
+    @IBOutlet weak var dowhileTxtView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        forTxtView.text = "";
+        whileTxtView.text = "";
+        dowhileTxtView.text = "";
+        
+        var i = 0;
+        for(i=0; i<10; i++){
+            forTxtView.text = forTxtView.text.stringByAppendingString("Hello~");
+        }
+        
+        i = 0;
+        while(i<10){
+            whileTxtView.text = whileTxtView.text.stringByAppendingString("你好！");
+        }
+        
+        //i = 0;
+      //  do{
+      //      dowhileTxtView.text = dowhileTxtView.text.stringByAppendingString("歐嗨呦～");
+      //      i++;
+      //  } while i<20
+        
     }
 
     override func didReceiveMemoryWarning() {
