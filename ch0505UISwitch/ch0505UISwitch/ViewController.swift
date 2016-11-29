58b25a4b1490d65267cc4273e0ddaabb3f7e054b
peijyun <peijyun.shih@gmail.com>
2016-11-29 22:16:53 +0800

    switch.on == true

--- ch0505UISwitch/ch0505UISwitch/ViewController.swift
+++ ch0505UISwitch/ch0505UISwitch/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var mySwitch: UISwitch!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func changeSwitch(sender: AnyObject) {
+        if(mySwitch.on){
+            myLabel.text = "Hello~"
+        } else{
+            myLabel.text = "哈囉:)"
+        }
+    }
 }
 
